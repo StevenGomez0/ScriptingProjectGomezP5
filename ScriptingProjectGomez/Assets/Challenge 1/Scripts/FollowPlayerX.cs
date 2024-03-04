b66c5b11db34e6b7092f7529499cc77fbe41e45a
Steven Gomez <719513@student.dusd.net>
2024-03-04 13:08:30 -0800

    3/4/24
    something with the horizontal and vertical axis and Input.GetAxis("Axis");

--- ScriptingProjectGomez/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ ScriptingProjectGomez/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,22 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOther : MonoBehaviour
+public class FollowPlayerX : MonoBehaviour
 {
-    public GameObject other;
+    public GameObject plane;
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.O)) 
-        {
-            Destroy(other);
-        }
+        transform.position = plane.transform.position + offset;
     }
 }
---
+++ ScriptingProjectGomez/Assets/Semester 2 Scripts/KeyInput.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour
+{
+    /*
+    public Image graphic;
+    public Sprite standard;
+    public Sprite downgfx;
+    public Sprite upgfx;
+    public Sprite heldgfx;
+    public Text boolDisplay1;
+    public Text boolDisplay2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
